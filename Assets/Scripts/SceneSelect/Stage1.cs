49055d4a56d04b6f66e5c8a570b88543f8cce8c4
TakumiTakanoTMCIT <m23094@g.metro-cit.ac.jp>
2023-09-30 18:03:28 +0900

    一時休憩

--- Assets/Scripts/SceneSelect/Stage1.cs
+++ Assets/Scripts/SceneSelect/Stage1.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class loadplayscene : MonoBehaviour
+public class Stage1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetMouseButtonDown(0))
-            SceneManager.LoadScene("PlayScene");*/      //  ©‰º‚Ì‚â‚è•û‚É‚¿‚å‚Á‚Æ•Ï‚¦‚Ä‚İ‚Ü‚µ‚½iŠ¾j
+        
     }
 
     public void BotanPush()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Stage1");
     }
 }
