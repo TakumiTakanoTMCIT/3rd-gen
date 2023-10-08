c3fc92fa1b4c31b3890818774fb18d914ebfb9f8
TakumiTakanoTMCIT <m23094@g.metro-cit.ac.jp>
2023-10-08 16:18:38 +0900

    アクションシーンからパズルシーンに移動するときに近くにいないとパズルシーンに転移しない様になりました。また、それのオンオフは変数で管理しているため、他の機能も簡単に追加できます

--- Assets/Scripts/GoPuzul.cs
+++ Assets/Scripts/GoPuzul.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class GoPuzul : MonoBehaviour
 {
+    public GameObject game;
+    public Sprite OnSprite , OffSprite;
+
+    bool isEnter;
+
+    SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = game.GetComponent<SpriteRenderer>();
+
+        spriteRenderer.sprite = OffSprite;
+
+        isEnter = false;
     }
 
     // Update is called once per frame
@@ -18,6 +28,30 @@ public class GoPuzul : MonoBehaviour
 
     public void OnClicked()//Playerが触れたらPuzulSceneに移動できます
     {
-        SaveInf.instance.IntoPuzulScene();
+        if(isEnter == true)
+        {
+            SaveInf.instance.IntoPuzulScene();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            //Debug.Log("ぱずる起動！！！");
+            spriteRenderer.sprite = OnSprite;
+
+            isEnter = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            spriteRenderer.sprite = OffSprite;
+
+            isEnter = false;
+        }
     }
 }
