a4c387c15d96a4c519ad308094467af194fbc7c5
TakumiTakanoTMCIT <m23094@g.metro-cit.ac.jp>
2023-10-24 17:01:07 +0900

    敵が正しい挙動で動くようになりました

--- Assets/Scripts/Enemy/EnemyCon.cs
+++ Assets/Scripts/Enemy/EnemyCon.cs
@@ -8,9 +8,6 @@ public class EnemyCon : MonoBehaviour
     public float speed = 1.0f;
     public float powerX = 1.0f;
 
-    [SerializeField]
-    float gravityScale;
-
     public EnemyImage enemyImage;
 
     GameObject enemymanager;
@@ -18,12 +15,9 @@ public class EnemyCon : MonoBehaviour
     SpriteRenderer sp;
     Rigidbody2D rb;
 
-    Vector3 gravity;
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(gravityScale);
         rb = this.GetComponent<Rigidbody2D>();
         enemymanager = GameObject.Find("EnemyDeathUIManager");
 
@@ -37,13 +31,9 @@ public class EnemyCon : MonoBehaviour
         // ワールド座標を基準に、座標を取得
         Vector3 worldPos = this.transform.position;
 
-        //worldPos.x += speed*powerX*Time.deltaTime;    // ワールド座標を基準にした、x座標を1に変更
+        worldPos.x += speed*powerX*Time.deltaTime;    // ワールド座標を基準にした、x座標を1に変更
 
-        gravity = new Vector3(gravityScale, 0f, 0f);
-        rb.AddForce(gravity);
-        Debug.Log(gravity + " " + gravityScale + this.gameObject.name);
-
-        //this.transform.position = worldPos;  // ワールド座標での座標を設定
+        this.transform.position = worldPos;  // ワールド座標での座標を設定
     }
 
     public void OnRight()
